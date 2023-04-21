7e3734d2ba209065b6d0ec9a2b9c0748f1f2b767
lexcsam <95624147+lexcsam@users.noreply.github.com>
2023-04-21 09:52:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,25 @@ from typing import Union
 from prediction import prediction
 from fastapi import FastAPI,Request
 import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "https://example.com",
+    "https://example.com:8443",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/")
 async def read_root(request: Request):
